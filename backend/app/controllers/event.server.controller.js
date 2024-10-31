d603ea0c4039ae42a7499f98a4b31d03d44f6de5
essinn <ehsan.raj@hotmail.com>
2024-10-31 20:25:18 +0000

    feat: implement event creation and get event by id feature

--- backend/app/controllers/event.server.controller.js
+++ backend/app/controllers/event.server.controller.js
@@ -1,9 +1,59 @@
 // this file will contain the validation schema for the event routes
 const Joi = require("joi");
+const event = require("../models/event.server.model");
 
-const createEvent = (req, res) => {};
-const eventById = (req, res) => {};
+const createEvent = (req, res) => {
+  const schema = Joi.object({
+    name: Joi.string().required(),
+    description: Joi.string().required(),
+    location: Joi.string().required(),
+    start_date: Joi.date().required(),
+    close_registration: Joi.date().required(),
+    max_attendees: Joi.number().required(),
+    creator_id: Joi.number().required(),
+  });
+
+  const { error } = schema.validate(req.body);
+
+  if (error) {
+    return res.status(400).send(error.details[0].message);
+  }
+
+  event.insert(req.body, (err, row) => {
+    if (err) {
+      return res
+        .status(400)
+        .send({ message: "Error creating event: " + err.message });
+    }
+
+    return res.status(201).send({ message: "Event created successfully", row });
+  });
+};
+
+const eventById = (req, res) => {
+  const schema = Joi.object({
+    event_id: Joi.number().required(),
+  });
+
+  const { error } = schema.validate(req.params);
+
+  if (error) {
+    return res.status(400).send(error.details[0].message);
+  }
+
+  event.select(req.params, (err, row) => {
+    if (err) {
+      return res
+        .status(400)
+        .send({ message: "Error getting events: " + err.message });
+    }
+
+    return res.status(200).json({ message: "Got events successfully", row });
+  });
+};
+
 const updateEvent = (req, res) => {};
+
 const attendee = (req, res) => {};
 const deleteEvent = (req, res) => {};
 const search = (req, res) => {};
--- backend/app/models/event.server.model.js
+++ backend/app/models/event.server.model.js
@@ -1,10 +1,47 @@
-// this file will contain the functions to handle the requests from the event routes
+/**
+ * this file will contain the functions to handle the requests from the event routes
+ */
+const db = require("../../database.js");
+
+// create a new event
+const insert = (event, done) => {
+  const sql =
+    "INSERT INTO events (name, description, location, start_date, close_registration, max_attendees, creator_id) VALUES (?, ?, ?, ?, ?, ?, ?)";
+
+  db.run(
+    sql,
+    [
+      event.name,
+      event.description,
+      event.location,
+      event.start_date,
+      event.close_registration,
+      event.max_attendees,
+      event.creator_id,
+    ],
+    (err, row) => {
+      if (err) {
+        done(err);
+      }
+
+      done(null, row);
+    }
+  );
+};
+
+const select = (event, done) => {
+  const sql = "SELECT * FROM events WHERE event_id = ?";
+
+  db.all(sql, [event.event_id], (err, row) => {
+    if (err) {
+      done(err);
+    }
+
+    done(null, row);
+  });
+};
 
 module.exports = {
-  createEvent,
-  eventById,
-  updateEvent,
-  attendee,
-  deleteEvent,
-  search,
+  insert,
+  select,
 };
